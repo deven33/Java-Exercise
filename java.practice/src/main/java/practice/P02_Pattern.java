6074037bf40caeb492a68eb4e80acf3681d749e9
deven33 <devendraameta33@gmail.com>
2021-04-25 21:15:50 +0530

    patteren

--- java.practice/src/main/java/practice/P02_Pattern.java
+++ java.practice/src/main/java/practice/P02_Pattern.java
@@ -12,6 +12,39 @@ public class P02_Pattern {
 		5 5 5 5 5
 */
 		createPattern1(n);
+		
+		
+		/*	1 
+			1 2 
+			1 2 3 
+			1 2 3 4 
+			1 2 3 4 5  */
+			
+			//pattern_1(n);
+			
+		/*	1 
+			2 1 
+			3 2 1 
+			4 3 2 1 
+			5 4 3 2 1  */
+			
+			//pattern_2(n);
+			
+		/*	A
+			AB
+			ABC
+			ABCD
+			ABCDE */
+			
+			//patteren_3(n);
+			
+		/*	ABCDE
+			ABCD
+			ABC
+			AB
+			A  */
+			
+			patteren_4(n);
 	}
 
 	private static void createPattern1(int n) {
@@ -28,6 +61,46 @@ public class P02_Pattern {
 			System.out.println();
 		}
 
+	}
+	
+	private static void patteren_4(int n) {
+		for (int i = n; i >= 1; i--) {
+			char ch ='A';
+			for(int j = 1; j<= i; j++) {
+				System.out.print(ch);
+				ch++;
+			}
+			System.out.println();
+		}		
+	}
+
+	private static void patteren_3(int n) {
+		for (int i = 1; i <= n; i++) {
+			char ch ='A';
+			for(int j = 1; j<= i; j++) {
+				System.out.print(ch);
+				ch++;
+			}
+			System.out.println();
+		}
+	}
+
+	private static void pattern_2(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static void pattern_1(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
--- java.practice/src/main/java/practice/Test.java
+++ java.practice/src/main/java/practice/Test.java
@@ -1,5 +1,78 @@
 package practice;
 
 public class Test {
-String str = "devenedra";
+	public static void main(String[] args) {
+		int n = 5;
+	/*	1 
+		1 2 
+		1 2 3 
+		1 2 3 4 
+		1 2 3 4 5  */
+		
+		//pattern_1(n);
+		
+	/*	1 
+		2 1 
+		3 2 1 
+		4 3 2 1 
+		5 4 3 2 1  */
+		
+		//pattern_2(n);
+		
+	/*	A
+		AB
+		ABC
+		ABCD
+		ABCDE */
+		
+		//patteren_3(n);
+		
+	/*	ABCDE
+		ABCD
+		ABC
+		AB
+		A  */
+		
+		patteren_4(n);
+	}
+
+	private static void patteren_4(int n) {
+		for (int i = n; i >= 1; i--) {
+			char ch ='A';
+			for(int j = 1; j<= i; j++) {
+				System.out.print(ch);
+				ch++;
+			}
+			System.out.println();
+		}		
+	}
+
+	private static void patteren_3(int n) {
+		for (int i = 1; i <= n; i++) {
+			char ch ='A';
+			for(int j = 1; j<= i; j++) {
+				System.out.print(ch);
+				ch++;
+			}
+			System.out.println();
+		}
+	}
+
+	private static void pattern_2(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static void pattern_1(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
 }
